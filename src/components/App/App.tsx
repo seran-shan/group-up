c1430c553528c4f8392c2afa8ccc397bdc932fcb
Seran Shanmugathas <Seran@live.no>
2022-02-22 17:13:50 +0100

    :lipstick: (#20) Added route for profileview

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,6 +7,7 @@ import NoUserRoute from '../routes/NoUserRoute';
 import PrivateRoute from '../routes/PrivateRoute';
 import DashboardView from '../../pages/DashboardView';
 import GroupsOverviewPage from '../../pages/GroupsOverview';
+import Profile from '../../pages/ProfileView';
 
 export const example = (x: number, y: number) => x + y;
 
@@ -17,11 +18,11 @@ const App = () => (
         <Route element={<NoUserRoute />}>
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUpView />} />
-          <Route path="/groups" element={<GroupsOverviewPage />} />
-
+          <Route path="/profile" element={<Profile />} />
         </Route>
         <Route element={<PrivateRoute />}>
           <Route path="/" element={<DashboardView />} />
+          <Route path="/groups" element={<GroupsOverviewPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
