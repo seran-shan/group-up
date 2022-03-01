3bdde807bcc364a768e998eaeabe32e90938b4cd
Thomhas <thomas.hasvold@gmail.com>
2022-03-01 20:04:27 +0100

    update profile page display

--- src/components/Profile/ProfileDetail.tsx
+++ src/components/Profile/ProfileDetail.tsx
@@ -1,179 +1,149 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { useAuth } from '../../provider/AuthProvider';
 import {
-    Box,
-    Button,
-    Card,
-    CardContent,
-    CardHeader,
-    Divider,
-    Grid,
-    TextField
-  } from '@mui/material';
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  Grid,
+  TextField,
+} from '@mui/material';
 import { User } from '../../types/profile';
-import { getUserByID } from '../../services/Firebase';
-import { LocalizationProvider } from '@mui/lab';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import FormDatePicker from '../molecules/FormDatePicker';
+import { createUser, getUserByID } from '../../services/Firebase';
+
+import FormTextField from '../molecules/FormTextField';
+
+import { useForm } from 'react-hook-form';
 
 const ProfileDetail = () => {
   const { user, signout } = useAuth();
-  const [frUser, setFrUser] = useState<User>()
+
+  const { register, handleSubmit, getValues } = useForm();
+
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [birthday, setBirthday] = useState<Date>(new Date());
+
   // const [date, setDate] = useState<string | unknown | null>(null);
 
   useEffect(() => {
-
-    if(user === null){
-      console.log("Filed to load user")
-        return
+    if (user === null) {
+      console.log('Filed to load user');
+      return;
     }
     getUserByID(user.uid).then((fUser) => {
-      if (fUser == null){
-        console.log("Can't find user in firestore")
-        return
+      if (fUser == null) {
+        console.log("Can't find user in firestore");
+        return;
       }
       var parsedUser = fUser as unknown as User;
-      setFrUser(parsedUser)
+      setFirstName(parsedUser.firstName);
+      setLastName(parsedUser.lastName);
+      setEmail(parsedUser.email);
+      setBirthday(parsedUser.birthday);
     });
-  });  
+  }, []);
 
-  // const handleChange = (event: React.FormEvent<HTMLFormElement>) => {
-    
-  // };
+  const onSubmit = handleSubmit(async () => {
+    if (user == null) {
+      return;
+    }
+    try {
+      await createUser(firstName, lastName, email, birthday, user?.uid);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
-  // const handleSave = (event: React.FormEvent<HTMLFormElement>) => {
-    
-  // };
+  const handleFirstName = (value: string) => {
+    setFirstName(value);
+  };
 
-  const handleLogout = async () => {
-    await signout();
+  const handleLastName = (value: string) => {
+    setLastName(value);
+  };
+
+  const handleEmail = (value: string) => {
+    setEmail(value);
   };
   return (
     <form
       autoComplete="off"
       noValidate
-    //   {...props}
+      onSubmit={onSubmit}
+      //   {...props}
     >
       <Card>
-        <CardHeader
-          subheader="Read only"
-          title="Profile"
-        />
+        <CardHeader subheader="Read only" title="Profile" />
         <Divider />
         <CardContent>
-          <Grid
-            container
-            spacing={3}
-          >
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
+          <Grid container spacing={3}>
+            <Grid item md={6} xs={12}>
+              <FormTextField
                 fullWidth
-                helperText="Please specify the first name"
                 label="First name"
                 name="firstName"
-                // onChange={(e) => setFrUser(frUser)}
-                required
-                value={frUser?.email}
+                disabled={true}
+                InputLabelProps={{ shrink: true }}
                 variant="outlined"
+                required
+                value={firstName}
+                onChange={(e) => handleFirstName(e.target.value)}
               />
             </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
+            <Grid item md={6} xs={12}>
+              <FormTextField
                 fullWidth
-                label="Last name"
+                label="Last Name"
                 name="lastName"
-                // onChange={handleChange}
-                required
-                value={frUser?.lastName}
+                InputLabelProps={{ shrink: true }}
                 variant="outlined"
+                disabled={true}
+                required
+                value={lastName}
+                onChange={(e) => handleLastName(e.target.value)}
               />
             </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
+            <Grid item md={12} xs={12}>
+              <FormTextField
                 fullWidth
-                label="Email Address"
+                label="Email"
                 name="email"
-                // onChange={handleChange}
-                required
-                value={frUser?.email}
+                InputLabelProps={{ shrink: true }}
                 variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                helperText="Please specify the first name"
-                label="First name"
-                name="firstName"
-                // onChange={(e) => setFrUser(frUser)}
+                disabled={true}
                 required
-                value={frUser?.birthday}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Country"
-                name="country"
-                // onChange={handleChange}
-                required
-                
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Email Address"
-                name="email"
-                // onChange={handleChange}
-                required
-                value={frUser?.email}
-                variant="outlined"
+                value={email}
+                onChange={(e) => handleEmail(e.target.value)}
               />
             </Grid>
           </Grid>
         </CardContent>
         <Divider />
-        {/* <Box
+        <Box
           sx={{
             display: 'flex',
-            justifyContent: 'flex-end',
-            p: 2
+            justifyContent: 'center',
+            p: 2,
           }}
         >
           <Button
-            color="primary"
+            type="submit"
+            fullWidth
             variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              backgroundColor: '#125A2E',
+              '&:hover': { backgroundColor: '#16913A' },
+            }}
           >
             Save details
           </Button>
-        </Box> */}
+        </Box>
       </Card>
     </form>
   );
