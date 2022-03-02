ce83be1021082aca7a9ce94b2c305b0221edea2c
Seran Shanmugathas <Seran@live.no>
2022-03-02 15:28:53 +0100

    :globe_with_meridians: Changed landing page to groups

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,6 @@ import './App.css';
 import { Routes, BrowserRouter, Route } from 'react-router-dom';
 import SignUpView from '../../pages/SignUpView';
 import SignIn from '../../pages/SignInView';
-import DashboardView from '../../pages/DashboardView';
 import Profile from '../../pages/ProfileView';
 import CreateGroupView from '../../pages/CreateGroupView';
 import GroupView from '../../pages/GroupView';
@@ -15,8 +14,7 @@ const App = () => (
   <div className="App">
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DashboardView />} />
-        <Route path="/groups" element={<GroupView />} />
+        <Route path="/" element={<GroupView />} />
         <Route path="/CreateGroup" element={<CreateGroupView />} />
         <Route path="/myGroups" element={<MyGroupsView />} />
         <Route path="/signin" element={<SignIn />} />
