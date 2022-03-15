8b5542ba3b76f765f1280c2325388318676ca0b4
helenaskants <69515184+helenaskants@users.noreply.github.com>
2022-03-15 13:44:20 +0100

    (#29) Added button to filter groups in groupoverview page

--- src/components/Groups/GroupOverview/GroupsOverview.tsx
+++ src/components/Groups/GroupOverview/GroupsOverview.tsx
@@ -1,3 +1,4 @@
+import { Button, Grid, Modal, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../../provider/AuthProvider';
@@ -8,7 +9,24 @@ import { Group } from '../../../types/group';
 import Navbar from '../../Navbar/Navbar';
 import GroupCard from '../GroupCard';
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 700,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 export default function GroupsOverview() {
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const [groups, setGroups] = useState<Group[]>();
   const { user } = useAuth();
 
@@ -37,7 +55,37 @@ export default function GroupsOverview() {
   return (
     <Box sx={{ flexGrow: 2 }}>
       <Navbar />
-
+      <Grid
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-around',
+          maxWidth: '1000px',
+          margin: 'auto',
+          marginTop: '40px',
+        }}>
+        <Button onClick={handleOpen}
+          variant="contained"
+          size="small"
+          style={{ background: '#33792F' }}
+        >Filter groups</Button>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h5" component="h2">
+              Filter groups
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              attributes you can filter
+            </Typography>
+          </Box>
+        </Modal>
+      </Grid>
       <Box
         sx={{
           display: 'flex',
