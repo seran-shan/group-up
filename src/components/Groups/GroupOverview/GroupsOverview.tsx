8d5dac6d4654934b5a7bf55a2b7dd41144a1cca8
helenaskants <69515184+helenaskants@users.noreply.github.com>
2022-03-15 15:55:45 +0100

    (#29) Added pop-up for filtering groups

--- src/components/Groups/GroupOverview/GroupsOverview.tsx
+++ src/components/Groups/GroupOverview/GroupsOverview.tsx
@@ -1,11 +1,13 @@
-import { Button, Grid, Modal, Typography } from '@mui/material';
+import { Button, Checkbox, FormControlLabel, FormGroup, Grid, InputLabel, MenuItem, Modal, Select, SelectChangeEvent, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import { useAuth } from '../../../provider/AuthProvider';
 
 import { getAllGroups } from '../../../services/Firebase';
 
 import { Group } from '../../../types/group';
+import FormTextField from '../../molecules/FormTextField';
 import Navbar from '../../Navbar/Navbar';
 import GroupCard from '../GroupCard';
 
@@ -23,12 +25,24 @@ const style = {
 
 export default function GroupsOverview() {
 
+  const [interests, setInterests] = React.useState<string[]>([]);
+
+  const handleCheckbox = (event: SelectChangeEvent) => {
+    setInterests((oldArray) => [...oldArray, event.target.value as string]);
+  };
+
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const { register } = useForm();
 
   const [groups, setGroups] = useState<Group[]>();
   const { user } = useAuth();
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
 
   const getGroup = async () => {
     const extraGroups: Group[] = [];
@@ -76,13 +90,95 @@ export default function GroupsOverview() {
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description"
         >
-          <Box sx={style}>
-            <Typography id="modal-modal-title" variant="h5" component="h2">
+          <Box sx={style} >
+            <Typography variant="h5" component="h2" sx={{ textAlign: 'center', m: 1 }}>
               Filter groups
             </Typography>
-            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-              attributes you can filter
-            </Typography>
+            <Box sx={{ textAlign: 'left', m: 1 }}>
+              <Grid>
+                <Typography sx={{ fontWeight: 'bold' }}>
+                  Location
+                </Typography>
+              </Grid>
+              <Typography sx={{ fontWeight: 'bold' }}>
+                Agelimit
+              </Typography>
+              <Grid item xs={12}>
+                <Select
+                  labelId="dropdown-age-label"
+                  id="dropdown-age"
+                  fullWidth
+                  value={age}
+                  onChange={handleChange}
+                >
+                  <MenuItem value={22}>18-25</MenuItem>
+                  <MenuItem value={27}>25-30</MenuItem>
+                  <MenuItem value={35}>30-40</MenuItem>
+                  <MenuItem value={50}>40-60</MenuItem>
+                  <MenuItem value={70}>60-80</MenuItem>
+                </Select>
+              </Grid>
+              <Typography sx={{ fontWeight: 'bold' }}>
+                Group size
+              </Typography>
+              <Typography sx={{ fontWeight: 'bold' }}>
+                Interests
+              </Typography>
+              <Grid sx={{ marginRight: 12, marginLeft: 5, textAlign: 'center' }} >
+                <FormGroup>
+                  <FormControlLabel
+                    control={
+                      <Checkbox color="success" onChange={handleCheckbox} />
+                    }
+                    label="Sports"
+                    value="Sports"
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox color="success" onChange={handleCheckbox} />
+                    }
+                    label="Music"
+                    value="Music"
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox color="success" onChange={handleCheckbox} />
+                    }
+                    label="Reading"
+                    value="Reading"
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox color="success" onChange={handleCheckbox} />
+                    }
+                    label="Cooking"
+                    value="Cooking"
+                  />
+                  <FormControlLabel
+                    control={
+                      <Checkbox color="success" onChange={handleCheckbox} />
+                    }
+                    label="Dancing"
+                    value="Dancing"
+                  />
+                </FormGroup>
+              </Grid>
+              <Typography sx={{ fontWeight: 'bold' }}>
+                Date
+              </Typography>
+              <Grid item xs={12} sm={6} md={6} >
+                <FormTextField
+                  id="datetime-local"
+                  required
+                  type="datetime-local"
+                  defaultValue="2022-02-24T10:30"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  {...register('date')}
+                />
+              </Grid>
+            </Box>
           </Box>
         </Modal>
       </Grid>
